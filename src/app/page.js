98c01c42bee50dd3bfafe0e3e69493f8ecae1870
kallolghosal <kghosal@gmail.com>
2024-02-07 14:51:26 +0530

    Auto deploy

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by git pull test&nbsp;
+          Get on by git pull test&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
