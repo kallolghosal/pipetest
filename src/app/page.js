65117704b29e93f95918049fb9d476594cee12a7
kallolghosal <kghosal@gmail.com>
2024-02-08 17:36:16 +0530

    Another update

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Automatic Deploy from Git&nbsp;
+          Automatic CI/CD pipe from Git&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
