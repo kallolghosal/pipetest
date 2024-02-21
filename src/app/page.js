d820da6714e0750985093e2bb51163b7f4250a47
kallolghosal <kghosal@gmail.com>
2024-02-21 13:39:40 +0530

    Port updated again

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className={styles.description}>
         <h1>Home</h1>
         <p>
-          Automatic CI/CD  test from Git&nbsp;
+          Automated CI/CD  test from Git&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
